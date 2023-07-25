06cfa4409f3d877e274720631ac94bb436cc6049
Oseyijotham <jothamoboh16@gmail.com>
2023-07-25 08:23:39 +0100

    Draft 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+const imageContainer = document.querySelector(".gallery");
+
+//console.log(galleryItems);
+let instance;
+galleryItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const gallryLink = document.createElement('a');
+    const pic = document.createElement('img');
+
+    listItem.setAttribute("class", "gallery__item");
+    gallryLink.setAttribute("class", "gallery__link");
+    gallryLink.setAttribute("href", item.original);
+    pic.setAttribute("class", "gallery__image");
+    pic.setAttribute("src", item.preview);
+    pic.setAttribute("alt", item.description);
+
+    gallryLink.append(pic);
+    listItem.append(gallryLink);
+    imageContainer.append(listItem);
+})
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    closeText: 'X',
+    animationSlide: false,
+});
+
+
+
